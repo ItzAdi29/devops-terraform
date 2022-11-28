0cbe5318cb88fffe21030405c7549bf422626e32
ItzAdi29 <91110158+ItzAdi29@users.noreply.github.com>
2022-11-28 18:07:25 +0000

    Update test sg ec2.tf
    updated key

--- test sg ec2.tf
+++ test sg ec2.tf
@@ -31,7 +31,7 @@ resource "aws_security_group" "private-sg" {
 resource "aws_instance" "my-EC2" {
   ami = "ami-0e6329e222e662a52"
   instance_type = "t2.micro"
-  key_name = "test-key"
+  key_name = "X21201188-AmzLinux"
   vpc_security_group_ids = [aws_security_group.private-sg.id]
   associate_public_ip_address = true
   root_block_device {
